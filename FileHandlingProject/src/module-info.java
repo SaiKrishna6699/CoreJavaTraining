a6aebdf916c3af0452afcf1990cf27bc806f5eab
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2022-03-03 12:23:41 +0530

    Adding FileHandling

---
+++ FileHandlingProject/src/module-info.java
@@ -0,0 +1,12 @@
+module FileHandlingProject {
+}
+
+/*
+ * 
+ * 	File Handling Project
+ *
+ *  Program by Rajagopal Sai Krishna
+ *  Git Id SaiKrishna6699
+ *  Git 'https://github.com/SaiKrishna6699/CoreJavaTraining.git'
+ * 
+ */
